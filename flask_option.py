36f3f683a511f0026ef7b4d6f7f9215bbfa71e5a
Macos <macos@192.168.1.103>
2026-02-12 13:03:06 +0300

    feat: complete environment variable challenge

--- flask_option.py
+++ flask_option.py
@@ -1,10 +1,17 @@
 # pylint: disable=missing-docstring
 
+"""
+This module demonstrates the usage of environment variables.
+"""
 import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    env = os.getenv("FLASK_ENV")
 
-if __name__ == "__main__":
-    print(start())
+    if env == "development":
+        return "Starting in development mode..."
+    if env == "production":
+        return "Starting in production mode..."
+
+    return "Starting in empty mode..."
